eadefbc68a3103321700210b94508592cdd16d43
David Santamaria <davidsantamaria1245@gmail.com>
2020-03-13 12:41:25 -0400

    finished simple udp echo server 2

--- client.java
+++ client.java
@@ -10,38 +10,51 @@ public class client {
   private byte[] buff;
   private static final int PORT = 33402;
 
-  public client() {
+  public client(String ip) {
     try {
-    socket = new DatagramSocket();
-    addr = InetAddress.getByName("localhost");
-    in = new Scanner(System.in);
+      socket = new DatagramSocket();
+      addr = InetAddress.getByName(ip);
+      System.out.println(addr);
+      in = new Scanner(System.in);
     } catch (SocketException se) {
       se.printStackTrace();
+    } catch (UnknownHostException uh) {
+      uh.printStackTrace();
     }
   }
 
   public void run() {
     while (true) {
-      String line = in.nextLine();
-      buff = line.getBytes();
-      DatagramPacket pack = new DatagramPacket(buff, buff.length, addr, PORT);
-      socket.send(pack);
-      pack = new DatagramPacket(buff, buff.length);
-      socket.receive(pack);
-      String recvd = new String(pack.getData(), 0, pack.getLength());
-      if (recvd.equals("exit")) {
-        close();
+      try {
+        String line = in.nextLine();
+        buff = line.getBytes();
+        DatagramPacket pack = new DatagramPacket(buff, buff.length, addr, PORT);
+        socket.send(pack);
+        pack = new DatagramPacket(buff, buff.length);
+        socket.receive(pack);
+        String recvd = new String(pack.getData(), 0, pack.getLength());
+        if (recvd.equals("exit")) {
+          close();
+        }
+        System.out.println(recvd);
+      } catch (IOException ioe) {
+        ioe.printStackTrace();
       }
-      System.out.println(recvd);
     }
   }
 
   public void close() {
-    sock.close();
+    socket.close();
     System.exit(0);
   }
 
   public static void main(String[] args) {
+    if (args.length < 1) {
+      System.err.println("USAGE: client [HOSTNAME]");
+      System.exit(1);
+    }
 
+    client c = new client(args[0]);
+    c.run();
   }
 }
